117282e84cd94453cd95e0cd7a1da94a1e6ea20d
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-14 13:07:25 -0300

    ++

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// function hello(){
+//     username = prompt('username por favor')
+//     console.log('hola' + username)
+
+// }
+// hello('gabriel')
+
+function hello(){
+    console.log('holamundod')
+
+}
+
+hello()
