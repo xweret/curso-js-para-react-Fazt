b76a6aca3c72bd8592ea4c029fb32a69fc137742
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-14 13:37:39 -0300

    objetos

--- index.js
+++ index.js
@@ -6,9 +6,44 @@
 // }
 // hello('gabriel')
 
-function hello(){
-    console.log('holamundod')
+// function hello(){
+//     return function (){
+//         return 'holamundo'
+//     }
 
+// }
+
+// console.log(hello()())
+
+// function saludar(name){
+//     return 'Buenas tardes, ' + name
+
+// }
+
+// console.log(saludar('Ryan'))
+
+// function sumar(x,y =0){
+//     return x + y 
+// } 
+
+// console.log(sumar(2))
+
+const user = {
+    x : 20,
+    name: 'Ryan',
+    lastname: 'perez',
+    age: '30',
+    adress: {
+        country: 'argentina',
+        city: 'chivilcoy',
+        street: 'yeca 34',
+    },
+    friends: ['brand', 'elena'],
+    active: true,
+    // x = key // 20 = value 
+    sendMail: function(){
+        return 'sending email...'
+    }
 }
 
-hello()
+console.log(user)
