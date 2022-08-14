207e52c83357d7ef0a8b539391c4c732b0d3d144
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-14 19:40:25 -0300

    dom

--- index.js
+++ index.js
@@ -28,11 +28,24 @@
 
 // console.log(sumar(2))
 
-const name = 'laptop'
-const price = 3000
+// const name = 'laptop'
+// const price = 3000
 
-const newProduct = {
-    name, price
+// const newProduct = {
+//     name, price
+// }
+
+// console.log(newProduct.name)
+
+const autos = {
+    ruedas: 'hola mundo desde js'
 }
-
-console.log(newProduct.name)
+const title = document.createElement('h1')
+title.innerText = autos.ruedas
+const button = document.createElement('button')
+button.innerText = 'click'
+button.addEventListener('click', function(){
+    console.log('hola mundo')
+})
+document.body.append(title)
+document.body.append(button)
