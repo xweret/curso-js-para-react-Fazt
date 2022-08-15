bb466dd8417fdce75d5272baa0faf5e19d393156
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-15 18:59:58 -0300

    return

--- index.js
+++ index.js
@@ -37,19 +37,92 @@
 
 // console.log(newProduct.name)
 
-const autos = {
-    ruedas: 'hola mundo desde js'
-}
-const title = document.createElement('h1')
-title.innerText = 'yo soy js'
+// const autos = {
+//     ruedas: 'hola mundo desde js'
+// }
+// const title = document.createElement('h1')
+// title.innerText = 'yo soy js'
+
+// const button = document.createElement('button')
+// button.innerText = 'click'
+
+// button.addEventListener('click', function () {
+//     title.innerText = 'Cambiado desde js'
+//     alert('se realizo un click')
+
+// })
+// document.body.append(title)
+// document.body.append(button)
+
+
+// Destructuring
+
+// const user = {
+//     name: 'Gabriel',
+//     age: 22,
+// }
+
+// function printInfo (user){
+// const { name, age } = user;
+// console.log(name, age);
+//     return '<h1>Hola ' + name +  '</h1>'
+// }
+// console.log(printInfo(user))
+// document.body.innerHTML = printInfo(user)
+
+// const button = document.createElement('button')
+// button.innerText = 'click me gato'
+
+// function handleClick(){
+//     alert('clicked')
+// }
+
+// button.addEventListener('click', handleClick)
+
+// document.body.append(button)
+
+// //add event listener con function anonima 
+
+// function sumar(y , x){
+//     return x + y
+// }
+
+// const sumarFlecha = (x, y) => {
+//     return x + y
+// }
+
+// console.log(sumarFlecha(20, 30))
+
+// const showBolean = () => 'i m a bolean' + true
+// const showNumber = () => 22
+// const showText = () => [1,2,3]
+// const showObjet = () => {
+//     ({name: 'ryan'})
+// }
+
+// console.log(showBolean())
+// console.log(showNumber())
+// console.log(showText())
+
+// console.log(showObjet())
+
+
+const isAuthorized = false 
 
 const button = document.createElement('button')
-button.innerText = 'click'
+button.innerText = 'click me gato'
 
-button.addEventListener('click', function () {
-    title.innerText = 'Cambiado desde js'
-    alert('se realizo un click')
+// console.log(showBolean())
 
-})
-document.body.append(title)
+
+
+button.addEventListener('click', () => { 
+    if (isAuthorized) {
+
+    return  alert('autorizao');
+    }
+        alert('notclicked');
+    });
+
 document.body.append(button)
+
